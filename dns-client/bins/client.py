b43c09f57fc3ca1c23156f7defc1cb7c76b0495c
Justin B <justin@penguintech.io>
2025-02-26 19:42:24 +0000

    Adding in client

---
+++ dns-client/bins/client.py
@@ -0,0 +1,61 @@
+#!/usr/bin/env python3
+
+import requests
+import json
+import sys
+import getopt
+
+class DNSOverHTTPSClient:
+    def __init__(self, dns_server_url="https://dns.google/dns-query", auth_token=None):
+        self.dns_server_url = dns_server_url
+        self.auth_token = auth_token
+
+    def query(self, domain, record_type="A"):
+        params = {
+            "name": domain,
+            "type": record_type
+        }
+        headers = {
+            "Accept": "application/dns-json"
+        }
+        if self.auth_token:
+            headers["Authorization"] = f"Bearer {self.auth_token}"
+        response = requests.get(self.dns_server_url, headers=headers, params=params)
+        if response.status_code == 200:
+            return response.json()
+        else:
+            response.raise_for_status()
+
+def main(argv):
+    domain = ''
+    record_type = 'A'
+    dns_server_url = "https://dns.google/dns-query"
+    auth_token = None
+    try:
+        opts, args = getopt.getopt(argv, "hd:t:s:a:", ["domain=", "type=", "server=", "auth="])
+    except getopt.GetoptError:
+        print('client.py -d <domain> -t <record_type> -s <dns_server_url> -a <auth_token>')
+        sys.exit(2)
+    for opt, arg in opts:
+        if opt == '-h':
+            print('client.py -d <domain> -t <record_type> -s <dns_server_url> -a <auth_token>')
+            sys.exit()
+        elif opt in ("-d", "--domain"):
+            domain = arg
+        elif opt in ("-t", "--type"):
+            record_type = arg
+        elif opt in ("-s", "--server"):
+            dns_server_url = arg
+        elif opt in ("-a", "--auth"):
+            auth_token = arg
+
+    if not domain:
+        print('Domain is required. Use -d <domain> to specify the domain.')
+        sys.exit(2)
+
+    client = DNSOverHTTPSClient(dns_server_url, auth_token)
+    result = client.query(domain, record_type)
+    print(json.dumps(result, indent=4))
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
